367ba092b27e9e0877c89138b03f2d266073e8af
kanaka vignesh <201001039@rajalakshmi.edu.in>
2025-12-25 15:41:08 +0530

    jj

--- locals.tf
+++ locals.tf
@@ -6,5 +6,5 @@ locals {
     }
 
     common_name_suffix = "${var.project_name}"     # roboshop-dev
-    az_zone_names = slice(data.aws_availability_zones.available.names,0,2 )      # using slice function concept to get only 1st 2 aws_availability_zone
+    #az_zone_names = slice(data.aws_availability_zones.available.names,0,2 )      # using slice function concept to get only 1st 2 aws_availability_zone
 }
